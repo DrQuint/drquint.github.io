a9816325cd3d1756568251556402fab5045b05b0
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2020-02-07 01:48:45 +0000

    Site update to static react build

---
+++ precache-manifest.70449bca5233625e2caa2bc2efc8b7d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134cd7d43cc832298237083295b27052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b754c9f0827937843ef",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b27643317e99e15a94c9",
+    "url": "/static/js/2.9983e312.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9983e312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b754c9f0827937843ef",
+    "url": "/static/js/main.f9078d72.chunk.js"
+  },
+  {
+    "revision": "6dc9d71e67ac3cd7bbe0",
+    "url": "/static/js/runtime-main.abd33880.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
